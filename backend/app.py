b8cc2b345cf9ffa6cabd99689e6d9e15bf3694c1
ehuang07 <huangsimiao2014@gmail.com>
2025-03-08 15:07:49 -0800

    pdf parsing endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.model_routes import model_bp
+from routes.upload_routes import upload_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,7 @@ CORS(app)
 app = Flask(__name__)
 
 app.register_blueprint(model_bp)
+app.register_blueprint(upload_bp)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
