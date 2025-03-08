9471018e7dae6c750f4d8a771b9cb291f9e6a5ea
ehuang07 <huangsimiao2014@gmail.com>
2025-03-08 14:43:08 -0800

    gemini text gen

---
+++ backend/models/gemini.py
@@ -0,0 +1,31 @@
+from flask import jsonify
+from .base_model import BaseModel
+import os
+from config.settings import GEMINI_KEY
+from google import genai
+
+class GenimiModel(BaseModel):
+  BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+  def __init__(self):
+    self.client = genai.Client(api_key=GEMINI_KEY)
+
+  def generate(self, input, input_type, output_type, options):
+    match output_type:
+      case "text":
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=[input])
+        print(response.text)
+
+        return jsonify(response.text)
+      
+      case "image":
+        return "Not yet implemented"
+      
+      case "audio":
+        return "Audio generation is not availible for Gemini"
+
+
+
+
